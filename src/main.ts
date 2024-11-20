31fb7e3e24de103da2d445e175fa896cf3387bcc
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2024-11-20 17:17:32 +0200

    feat: added validation pipe to the app. feat: loaded dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
 
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
-  await app.listen(process.env.PORT ?? 8080);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+    }),
+  );
+
+
+  await app.listen(process.env.APP_PORT ?? 8080, '0.0.0.0');
 }
 bootstrap();
